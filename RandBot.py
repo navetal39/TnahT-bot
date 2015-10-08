ff82b609e8739439a02825a307646cee91509b01
Nave <navetal@gmail.com>
2015-10-09 00:19:37 +0300

    Fixed RandBot

--- RandBot.py
+++ RandBot.py
@@ -1,13 +1,16 @@
-def do_Turn(pw):
+def do_turn(pw):
     if len(pw.my_fleets()) >= 1:
         return
     if len(pw.my_planets()) == 0:
         return
     if len(pw.neutral_planets()) >= 1:
         dest = pw.neutral_planets()[0]
+        source = pw.my_planets()[0]
+        num_ships = source.num_ships() / 2
+        pw.issue_order(source, dest, num_ships)
     elif len(pw.enemy_planets()) >= 1:
         dest = pw.enemy_planets()[0]
-        source = pw.my_planets()
+        source = pw.my_planets()[0]
         num_ships = source.num_ships() / 2
-        ow.issue_order(source, dest, num_ships)
+        pw.issue_order(source, dest, num_ships)
              
